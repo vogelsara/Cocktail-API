ad880d016d55e4464835fd8507a88774d7e46944
Sari <sari.vogel.94@gmail.com>
2020-06-23 22:28:23 +0200

    Get a random cocktail

--- public/script.js
+++ public/script.js
@@ -1,11 +1,35 @@
 
-    class App extends React.Component {
-        render() {
-            
-        }
-  }
+class App extends React.Component {
+    constructor(props) {
+        super(props);
 
-  ReactDOM.render(
+        this.getRandomCocktail = this.getRandomCocktail.bind(this);
+    }
+
+    getRandomCocktail(e) {
+        
+        var xhttp = new XMLHttpRequest();
+
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                var randomCocktail = JSON.parse(this.responseText);
+                console.log(randomCocktail);
+            }
+        }
+
+        xhttp.open("GET", "https://www.thecocktaildb.com/api/json/v1/1/random.php", true);
+
+        xhttp.send();
+    }
+    
+    render() {
+        return (
+            <button onClick={this.getRandomCocktail}></button>
+        )
+    }
+}
+
+ReactDOM.render(
     <App />,
     document.getElementById('main')
 );
--- server.js
+++ server.js
@@ -2,5 +2,6 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 app.use(express.static('./public'));
+const cocktails = [];
 
 app.listen(3000, () => console.log('server is listening'));
